a628f603cb9d13ea46a05c43c1a0726fba99d741
Abraham Cordero <abrahamicm2@gmail.com>
2023-12-12 16:08:04 -0400

    .

--- 14_mis_script/javascript/css.js
+++ 14_mis_script/javascript/css.js
@@ -1,15 +1,64 @@
-//  ver el nombre de las clases css disponibles en el documento
-var styleSheets = document.styleSheets;
-for (var i = 0; i < styleSheets.length; i++) {
-  var sheet = styleSheets[i];
+function listarClasesCSS() {
+  var styleSheets = document.styleSheets;
 
-  for (var j = 0; j < sheet.cssRules.length; j++) {
-    var rule = sheet.cssRules[j];
+  for (var i = 0; i < styleSheets.length; i++) {
+    var sheet = styleSheets[i];
 
-    if (rule.selectorText && rule.selectorText.indexOf('.') === 0) {
-      var className = rule.selectorText.slice(1);
+    for (var j = 0; j < sheet.cssRules.length; j++) {
+      var rule = sheet.cssRules[j];
 
-      console.log(className);
+      if (rule.selectorText && rule.selectorText.indexOf('.') === 0) {
+        var className = rule.selectorText.slice(1);
+
+        console.log(className);
+      }
     }
   }
 }
+
+// listarClasesCSS();
+
+
+function obtenerInformacionFuentes() {
+  var allElements = document.querySelectorAll('*');
+  var fontInfo = {};
+  allElements.forEach(function (element) {
+    var computedStyle = window.getComputedStyle(element);
+    var fontFamily = computedStyle.fontFamily;
+    var fontSize = computedStyle.fontSize;
+    if (fontFamily && fontSize) {
+      if (!fontInfo[fontFamily]) {
+        fontInfo[fontFamily] = [];
+      }
+      fontInfo[fontFamily].push(fontSize);
+    }
+  });
+
+  console.log('Información de fuentes y tamaños de texto:');
+  console.log(fontInfo);
+  return fontInfo;
+}
+
+// obtenerInformacionFuentes();
+
+
+function buscarClasesConPropiedad(propiedadCSS) {
+  var styleSheets = document.styleSheets;
+
+  for (var i = 0; i < styleSheets.length; i++) {
+    var sheet = styleSheets[i];
+
+    for (var j = 0; j < sheet.cssRules.length; j++) {
+      var rule = sheet.cssRules[j];
+
+      if (rule.selectorText && rule.style && rule.style[propiedadCSS]) {
+        var className = rule.selectorText.slice(1);
+
+        console.log(`Clase con ${propiedadCSS}: ${className}`);
+      }
+    }
+  }
+}
+
+// buscarClasesConPropiedad('display');
+
