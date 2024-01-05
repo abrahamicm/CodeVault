5ef6ba94ffe69fc0f91bc594033a35ebe01c8334
Abraham Cordero <32270874+abrahamicm@users.noreply.github.com>
2024-01-05 14:20:22 -0400

    Update css.js

--- 14_mis_script/javascript/css.js
+++ 14_mis_script/javascript/css.js
@@ -13,3 +13,33 @@ for (var i = 0; i < styleSheets.length; i++) {
     }
   }
 }
+
+
+function obtenerBreakpoints() {
+    // Obtén todas las hojas de estilo en el documento
+    var hojasDeEstilo = document.styleSheets;
+
+    // Recorre cada hoja de estilo
+    for (var i = 0; i < hojasDeEstilo.length; i++) {
+        var reglasDeEstilo;
+        
+        try {
+            // Algunas hojas de estilo pueden tener reglas bloqueadas por políticas de CORS
+            reglasDeEstilo = hojasDeEstilo[i].cssRules || hojasDeEstilo[i].rules;
+        } catch (error) {
+            console.error('No se puede acceder a las reglas de estilo de la hoja de estilo:', hojasDeEstilo[i]);
+            continue;
+        }
+
+        // Recorre cada regla de estilo
+        for (var j = 0; j < reglasDeEstilo.length; j++) {
+            // Verifica si la regla es una media query
+            if (reglasDeEstilo[j].type === CSSRule.MEDIA_RULE) {
+                console.log('Media Query:', reglasDeEstilo[j].media.mediaText);
+            }
+        }
+    }
+}
+
+// Llama a la función para obtener los breakpoints
+obtenerBreakpoints();
